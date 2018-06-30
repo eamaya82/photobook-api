cb0ce752b9ead11afdd22eac3a506edfef9a9466
Edgardo Amaya <eamaya82@gmail.com>
2018-06-30 12:06:37 -0500

    Versioning API V1

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
+const api = require('./api/v1/');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.use('/api/v1', api);
+app.use('/api', api);
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.json({
+    message: err.message,
+  });
 });
 
 module.exports = app;
